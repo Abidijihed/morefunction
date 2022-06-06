0b6873a320b0192abc90bb6af92d4005bac9374c
Abidijihed <abidij55@gmail.com>
2022-06-06 09:10:16 +0100

    last practice before move on to the object

---
+++ functions.js
@@ -0,0 +1,31 @@
+//1.Write a JavaScript program to check whether a given positive number is a multiple of 3 or a multiple of 7.
+function name(){
+
+}
+//2.Write a JavaScript program to replace every character in a given string with the character following it in the alphabet.
+function LetterChanges(){
+
+}
+//3.Write a JavaScript program to capitalize the first letter of each word of a given string.
+
+function capital_letter() {
+
+}
+//4.Write a JavaScript program to check whether 1 appears in first or last position of a given array of integers. The array length must be greater or equal to 1.
+function first_last_1()
+{
+  
+}
+
+//5. Write a JavaScript program to find the number of sorted pairs formed by its elements of a given array of integers such that one element in the pair is divisible by the other one.
+
+// For example - The output of [1, 3, 2] ->2 - (1,3), (1,2).
+// The output of [2, 4, 6] -> 2 - (2,4), (2,6)
+// The output of [2, 4, 16] -> 3 - (2,4), (2,16), (4,16)
+function arr_pairs() {
+}
+//6. Write a JavaScript program to check whether a matrix is a diagonal matrix or not. In linear algebra, a diagonal matrix is a matrix in which the entries outside the main diagonal are all zero (the diagonal from the upper left to the lower right).
+
+// Example:
+// [1, 0, 0], [0, 2, 0], [0, 0, 3] ]) = true
+// [1, 0, 0], [0, 2, 3], [0, 0, 3] ]) = false
